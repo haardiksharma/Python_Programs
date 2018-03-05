b00c132091e4597109a95decf1cf8a760b7a3360
Haardik Sharma <haardik.sharma.52@gmail.com>
2018-03-04 16:59:39 -0800

    Update Even_Odd.py

--- Even_Odd.py
+++ Even_Odd.py
@@ -3,6 +3,6 @@ def is_even(x):
     #check whether if the number is divisible by 2, if yes then the number is even
 	if x%2 == 0:
         return True
-    #if not then number is bouond to be a odd
+    #if not then this number is bound to be a odd
 	else:
         return False
